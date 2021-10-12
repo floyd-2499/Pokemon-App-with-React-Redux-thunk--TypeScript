b0c56531b01113646ac7e07401d0fec29a2f87a2
Floyd Manuel <floydmanuel011@gmail.com>
2021-10-12 23:52:18 +0530

    done with project -Pokemon- React-Redux-TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootStore } from "./Store";
+import {GetPokemon} from './actions/PokemonActions'
 
 function App() {
+  const dispatch = useDispatch()
+  const [pokemonName, setPokemonName] = useState("");
+
+  const pokemonState = useSelector((state: RootStore) => state.pokemon);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPokemonName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    dispatch(GetPokemon(pokemonName))
+  }
+  console.log(pokemonState);
+
   return (
     <div className="App">
+      <input type="text" onChange={handleChange} />
+      <button onClick={handleSubmit}>Search</button>
+
+{pokemonState.pokemon && (
+  <div>
+    <img src={pokemonState.pokemon.sprites.front_default} alt="" />
+    {pokemonState.pokemon.abilities.map(ability => {
+      return <p key={ability.ability.name}>{ability.ability.name}</p>
+    })}
+  </div>
+)}
 
     </div>
   );
